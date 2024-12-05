b079ef63946271ab6cfda9774085582497298b67
rogerguinon <133911508+rogerguinon@users.noreply.github.com>
2024-12-05 22:58:29 +0100

    Arreglo bug con USD

--- challenge2-req02.sql
+++ challenge2-req02.sql
@@ -10,10 +10,14 @@ BEGIN
     DECLARE currency_exists BOOLEAN;
     DECLARE currency_code_uppercase VARCHAR(3);
     SET currency_code_uppercase = UPPER(currency_code);
-    SELECT COUNT(*) > 0 INTO currency_exists
-    FROM information_schema.COLUMNS
-    WHERE TABLE_NAME = 'fx_from_usd' AND COLUMN_NAME = currency_code_uppercase
-    OR TABLE_NAME = 'fx_to_usd' AND COLUMN_NAME = currency_code_uppercase;
-    RETURN currency_exists;
+    IF currency_code_uppercase = 'USD' THEN
+        SET currency_exists = TRUE;
+    ELSE
+        SELECT COUNT(*) > 0 INTO currency_exists
+        FROM information_schema.COLUMNS
+        WHERE TABLE_NAME = 'fx_from_usd' AND COLUMN_NAME = currency_code_uppercase
+        OR TABLE_NAME = 'fx_to_usd' AND COLUMN_NAME = currency_code_uppercase;
+        RETURN currency_exists;
+    END IF;
 END
 //
