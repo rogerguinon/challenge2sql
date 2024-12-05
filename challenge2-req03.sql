85008a3c5e2752eecc3d40f584ce8611cbc59bc9
rogerguinon <133911508+rogerguinon@users.noreply.github.com>
2024-12-05 21:32:16 +0100

    Update challenge2-req03.sql

--- challenge2-req03.sql
+++ challenge2-req03.sql
@@ -3,41 +3,34 @@ returns in another parameter FALSE if the given date is not present in conversio
 both tables) or the value for our specified currency and date is 0 or null. Return TRUE otherwise.
 Hint: you may need to use a prepared statement.*/
 
-DELIMITER //
-
 DROP PROCEDURE IF EXISTS validate_date_currency;
-
+DELIMITER //
 CREATE PROCEDURE validate_date_currency(
     IN check_date DATE,
     IN currency_code VARCHAR(3),
     OUT is_valid BOOLEAN
 )
 BEGIN
-    DECLARE count_entries INT;
+    DECLARE count INT;
     SET is_valid = FALSE;
-
-    -- Prepared statement to dynamically query both conversion tables
     SET @query = CONCAT(
-        "SELECT COUNT(*) INTO @count_entries FROM conversion_table_1 WHERE date_field = ? AND `",
-        currency_code, "` > 0 AND `", currency_code, "` IS NOT NULL"
+        "SELECT COUNT(*) INTO @count FROM fx_from_usd WHERE date = ? AND `", currency_code, "` > 0 AND `", currency_code, "` IS NOT NULL"
     );
 
     PREPARE stmt FROM @query;
-    EXECUTE stmt USING check_date;
-
-    IF @count_entries > 0 THEN
+    EXECUTE stmt USING @check_date;
+	SELECT count;
+    IF @count > 0 THEN
         SET is_valid = TRUE;
     ELSE
-        -- Repeat for second conversion table
         SET @query = CONCAT(
-            "SELECT COUNT(*) INTO @count_entries FROM conversion_table_2 WHERE date_field = ? AND `",
-            currency_code, "` > 0 AND `", currency_code, "` IS NOT NULL"
+            "SELECT COUNT(*) INTO @count FROM fx_to_usd WHERE date = ? AND `", currency_code, "` > 0 AND `", currency_code, "` IS NOT NULL"
         );
 
         PREPARE stmt FROM @query;
-        EXECUTE stmt USING check_date;
+        EXECUTE stmt USING @check_date;
 
-        IF @count_entries > 0 THEN
+        IF @count > 0 THEN
             SET is_valid = TRUE;
         END IF;
     END IF;
