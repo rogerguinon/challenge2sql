29d8609c0a82ee79e094fe80598759a920a58df5
rogerguinon <133911508+rogerguinon@users.noreply.github.com>
2024-12-05 23:01:21 +0100

    Arreglo bug con USD

--- challenge2-req02.sql
+++ challenge2-req02.sql
@@ -1,6 +1,3 @@
---Create a new function that given a currency code returns TRUE if the currency code exists or FALSE if it does not exist. Input should always be capitalised once we receive the parameter.
--- Hint: For a currency code to exist, it should be USD or appear in any of the two conversion tables where you import your CSV as a column name.
--- Hint: use information_schema.COLUMNS
 DROP FUNCTION IF EXISTS currency_exists;
 DELIMITER //
 CREATE FUNCTION currency_exists (currency_code VARCHAR(3))
@@ -17,7 +14,7 @@ BEGIN
         FROM information_schema.COLUMNS
         WHERE TABLE_NAME = 'fx_from_usd' AND COLUMN_NAME = currency_code_uppercase
         OR TABLE_NAME = 'fx_to_usd' AND COLUMN_NAME = currency_code_uppercase;
-        RETURN currency_exists;
     END IF;
+    RETURN currency_exists;
 END
 //
