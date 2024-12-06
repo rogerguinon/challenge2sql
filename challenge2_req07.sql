b6ffa55436764f0482a27436314da1e559b1d64b
rogerguinon <133911508+rogerguinon@users.noreply.github.com>
2024-12-06 19:11:16 +0100

    Arreglada

--- challenge2_req07.sql
+++ challenge2_req07.sql
@@ -1,21 +1,21 @@
-DROP FUNCTION IF EXISTS req07_get_preferred_currency;
+DROP FUNCTION IF EXISTS get_preferred_currency;
 DELIMITER //
-CREATE FUNCTION req07_get_preferred_currency (id_person INT)
+CREATE FUNCTION get_preferred_currency (id_person INT)
 RETURNS VARCHAR(3)
 
 BEGIN
     DECLARE nationality VARCHAR(50);
-    DECLARE eurozones TEXT DEFAULT 'Austria, Belgium, Croatia, Cyprus, Estonia, Finland, France, Germany, Greece, Ireland, Italy, Latvia, Lithuania, Luxembourg, Malta, Netherlands, Portugal, Slovakia, Slovenia, Spain'; --Check the data if the countries are written in the same way
+    DECLARE eurozones TEXT DEFAULT 'Austria, Belgium, Croatia, Cyprus, Estonia, Finland, France, Germany, Greece, Ireland, Italy, Latvia, Lithuania, Luxembourg, Malta, Netherlands, Portugal, Slovakia, Slovenia, Spain';
 
     SELECT nationality INTO nationality
     FROM festivalgoer
-    WHERE id_festilvalgoer = id_person
+    WHERE id_festivalgoer = id_person;
 
     IF nationality = 'United States' THEN
         RETURN 'USD';
     ELSEIF nationality = 'Canada' THEN
         RETURN 'CAD';
-    ELSEIF nationality = 'United Kingdom' THEN --Maybe it is better to use 'U.K.' instead of 'United Kingdom' (check the data)
+    ELSEIF nationality = 'United Kingdom' THEN
         RETURN 'GBP';
     ELSEIF nationality = 'Japan' THEN
         RETURN 'JPY';
@@ -29,6 +29,3 @@ BEGIN
 END //
 
 DELIMITER ;
-
---Example usage:
---SELECT req07_get_preferred_currency(1); -- Example with id_person = 1
