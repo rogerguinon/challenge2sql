a01d589d39ace123e02e7ca73cd67697356ed589
rogerguinon <133911508+rogerguinon@users.noreply.github.com>
2024-12-06 19:26:51 +0100

    Update challenge2-req03.sql

--- challenge2-req03.sql
+++ challenge2-req03.sql
@@ -1,8 +1,3 @@
-/*Create a new procedure that given two parameters (date to be checked, and currency code)
-returns in another parameter FALSE if the given date is not present in conversion tables (check
-both tables) or the value for our specified currency and date is 0 or null. Return TRUE otherwise.
-Hint: you may need to use a prepared statement.*/
-
 DROP PROCEDURE IF EXISTS validate_date_currency;
 DELIMITER //
 CREATE PROCEDURE validate_date_currency(
