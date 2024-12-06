e439501864369dadeaa05596beba2fdb767203fb
rogerguinon <133911508+rogerguinon@users.noreply.github.com>
2024-12-06 20:19:32 +0100

    Arreglo bug EUR

--- challenge2_req07.sql
+++ challenge2_req07.sql
@@ -1,27 +1,26 @@
 DROP FUNCTION IF EXISTS get_preferred_currency;
 DELIMITER //
-CREATE FUNCTION get_preferred_currency (id_person INT)
+CREATE FUNCTION get_preferred_currency (id INT)
 RETURNS VARCHAR(3)
 
 BEGIN
-    DECLARE nationality VARCHAR(50);
-    DECLARE eurozones TEXT DEFAULT 'Austria, Belgium, Croatia, Cyprus, Estonia, Finland, France, Germany, Greece, Ireland, Italy, Latvia, Lithuania, Luxembourg, Malta, Netherlands, Portugal, Slovakia, Slovenia, Spain';
+    DECLARE country VARCHAR(50);
+    DECLARE eurozones TEXT DEFAULT 'Austria,Belgium,Croatia,Cyprus,Estonia,Finland,France,Germany,Greece,Ireland,Italy,Latvia,Lithuania,Luxembourg,Malta,Netherlands,Portugal,Slovakia,Slovenia,Spain';
 
-    SELECT nationality INTO nationality
-    FROM festivalgoer
-    WHERE id_festivalgoer = id_person;
+    SELECT nationality INTO country
+    FROM person WHERE id_person = id;
 
-    IF nationality = 'United States' THEN
+    IF country = 'United States' THEN
         RETURN 'USD';
-    ELSEIF nationality = 'Canada' THEN
+    ELSEIF country = 'Canada' THEN
         RETURN 'CAD';
-    ELSEIF nationality = 'United Kingdom' THEN
+    ELSEIF country = 'United Kingdom' THEN
         RETURN 'GBP';
-    ELSEIF nationality = 'Japan' THEN
+    ELSEIF country = 'Japan' THEN
         RETURN 'JPY';
-    ELSEIF FIND_IN_SET(nationality, eurozones) <> 0 THEN 
+    ELSEIF FIND_IN_SET(country, eurozones) <> 0 THEN 
         RETURN 'EUR';
-    ELSEIF nationality IS NULL THEN
+    ELSEIF country IS NULL THEN
         RETURN 0;
     ELSE
         RETURN NULL;
